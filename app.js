19ed4b835c39604d430f277e72299fb11489fd1a
dinarrene <dinardavis@gmail.com>
2021-02-03 20:18:53 -0800

    create root, data.json, index and about route, link to imgs

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const path = require('path');
+const { projects } = require('./data/data.json');
+
 
 const app = express();
 
+// View engine setup
+app.set('view engine', 'pug');
+
+// Add static middleware
+app.use( '/static', express.static('public'));
 
 
-app.get('/', (req, res) => {
-    res.send('A string!');
+// GET home page
+app.get('/', function(req, res, next) {
+    res.render('index', { projects });
 });
+
+// GET projects page
+app.get('/projects/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId)
+    if (project) {
+        res.render('project', { project });
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+// Get about page
+app.get('/about', function(req, res, next) {
+    res.render('about');
+});
+
 
 
 app.listen(3000, () => {
